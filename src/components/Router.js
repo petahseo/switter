dba59f15628f725bc6504ecc343818c3d05f356c
petahseo <petahseo@gmail.com>
2022-05-19 16:52:13 +0900

    upto authentication

---
+++ src/components/Router.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
+
+const AppRouter = ({ isLoggedIn }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/profile" element={<Profile />}></Route>
+          </>
+        ) : (
+          <>
+            <Route exact path="/" element={<Auth />}></Route>
+          </>
+        )}
+      </Routes>
+    </Router>
+
+  )
+}
+
+export default AppRouter;
---
+++ src/routes/Home.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  const [sweet, setSweet] = useState("")
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+  const onChange = (event) => {
+    const { target: { value } } = event
+    setSweet(value)
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={sweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="Sweet" />
+      </form>
+    </div>
+  )
+}
+export default Home;
